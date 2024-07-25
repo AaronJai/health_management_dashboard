e40f90a3b105bd215f55b886dcc20ba33bcc58f3
Aaron <aaron.tan1@gmail.com>
2024-07-25 16:07:01 +0800

    feat: implement personal information section of registration page

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -16,6 +16,9 @@ import Image from "next/image";
 import 'react-phone-number-input/style.css'
 import PhoneInput from 'react-phone-number-input'
 import { E164Number } from 'libphonenumber-js/core';
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 interface CustomProps {
     control: Control<any>,
@@ -33,7 +36,7 @@ interface CustomProps {
 }
 
 const RenderField = ({ field, props }: {field:any; props: CustomProps}) => {
-    const { fieldType, iconSrc, iconAlt, placeholder, } = props;
+    const { fieldType, iconSrc, iconAlt, placeholder, showTimeSelect, dateFormat, renderSkeleton } = props;
 
     switch (fieldType) {
         case FormFieldType.INPUT:
@@ -74,6 +77,33 @@ const RenderField = ({ field, props }: {field:any; props: CustomProps}) => {
                     />
                 </FormControl>
             )
+        
+        case FormFieldType.DATE_PICKER:
+            return (
+                <div className="flex rounded-md border border-dark-500 bg-dark-400">
+                    <Image
+                        src="/assets/icons/calendar.svg"
+                        alt="calendar"
+                        height={24}
+                        width={24}
+                        className="ml-2"
+                    />
+                    <FormControl>
+                        <DatePicker 
+                            selected={field.value} 
+                            onChange={(date) => field.onChange(date)} 
+                            dateFormat={dateFormat ?? 'dd/MM/yyyy'}
+                            showTimeSelect={showTimeSelect ?? false}
+                            timeInputLabel="Time: "
+                            wrapperClassName="date-picker"
+                        />
+                    </FormControl>
+                </div>
+            )
+
+        case FormFieldType.SKELETON:
+            return renderSkeleton ? renderSkeleton(field) : null
+            
         default:
             break;
     }
---
+++ components/forms/RegisterForm.tsx
@@ -0,0 +1,196 @@
+"use client"
+ 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import { Form, FormControl } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import CustomFormField from "../CustomFormField"
+import SubmitButton from "../SubmitButton"
+import { useState } from "react"
+import { UserFormValidation } from "@/lib/validation"
+import { useRouter } from "next/navigation"
+import { createUser } from "@/lib/actions/patient.actions"
+import { FormFieldType } from "./PatientForm"
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group"
+import { GenderOptions } from "@/constants"
+import { Label } from "@/components/ui/label"
+
+const RegisterForm = ({ user }: {user: User}) => {
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(false);
+
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof UserFormValidation>>({
+        resolver: zodResolver(UserFormValidation),
+        defaultValues: {
+            name: "",
+            email: "",
+            phone: ""
+        },
+    })
+    
+    // 2. Define a submit handler.
+    async function onSubmit({ name, email, phone}: z.infer<typeof UserFormValidation>) {
+        // Do something with the form values.
+        setIsLoading(true);
+
+        try {
+            const userData = { name, email, phone };
+
+            const user = await createUser(userData);
+
+            console.log(user);
+
+            if (user) router.push(`/patients/${user.$id}/register`)
+
+        }   catch (error) {
+            console.error(error)
+        }
+        setIsLoading(false);
+    }
+    
+    return (
+        <Form {...form}>
+            {/* Welcome description */}
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-12 flex-1">
+                <section className="space-y-4">
+                    <h1 className="header">Welcome!</h1>
+                    <p className="text-dark-700">Tell us know more about yourself.</p>
+                </section>
+
+                <section className="space-y-6">
+                    <div className="mb-9 space-y-1">
+                        <h2 className="sub-header">Personal Information</h2>
+                    </div>
+                </section>
+
+                {/* NAME */}
+                <CustomFormField
+                    fieldType={FormFieldType.INPUT}
+                    control={form.control}
+                    name="name"
+                    label="Full name"
+                    placeholder="John Doe"
+                    iconSrc="/assets/icons/user.svg"
+                    iconAlt="user"
+                />
+
+                {/* EMAIL AND PHONE NUMBER */}
+                <div className="flex flex-col gap-6 xl:flex-row">
+                    <CustomFormField
+                        fieldType={FormFieldType.INPUT}
+                        control={form.control}
+                        name="email"
+                        label="Email"
+                        placeholder="john.doe1@gmail.com"
+                        iconSrc="/assets/icons/email.svg"
+                        iconAlt="email"
+                    />
+
+                    <CustomFormField
+                        fieldType={FormFieldType.PHONE_INPUT}
+                        control={form.control}
+                        name="phone"
+                        label="Phone Number"
+                        placeholder="(+61) 412-345-678"
+                    />
+                </div>
+
+                {/* DATE OF BIRTH AND GENDER */}
+                <div className="flex flex-col gap-6 xl:flex-row">
+                    <CustomFormField
+                        fieldType={FormFieldType.DATE_PICKER}
+                        control={form.control}
+                        name="birthDate"
+                        label="Date of Birth"
+                        placeholder="DD/MM/YYYY"
+                    />
+
+                    <CustomFormField
+                        fieldType={FormFieldType.SKELETON}
+                        control={form.control}
+                        name="gender"
+                        label="Gender"
+                        renderSkeleton={(field) => (
+                            <FormControl>
+                                <RadioGroup
+                                    className="flex h-11 gap-6 xl:justify-between"
+                                    onValueChange={field.onChange}
+                                    defaultValue={field.value}>
+                                        {GenderOptions.map((option, i) => (
+                                            <div key={option + i} className="radio-group">
+                                                <RadioGroupItem value={option} id={option} />
+                                                <Label htmlFor={option} className="cursor-pointer">
+                                                {option}
+                                                </Label>
+                                            </div>
+                                        ))}
+                                </RadioGroup>
+                            </FormControl>
+                        )}
+                    />
+                </div>
+                
+                
+                {/* ADDRESS AND OCCUPATION */}
+                <div className="flex flex-col gap-6 xl:flex-row">
+                    <CustomFormField
+                        fieldType={FormFieldType.INPUT}
+                        control={form.control}
+                        name="address"
+                        label="Address"
+                        placeholder="123 ABC Street"
+                    />
+
+                    <CustomFormField
+                        fieldType={FormFieldType.INPUT}
+                        control={form.control}
+                        name="occupation"
+                        label="Occupation"
+                        placeholder="Software Engineer"
+                    />
+                </div>
+                
+                {/* EMERGENCY CONTACT */}
+                <div className="flex flex-col gap-6 xl:flex-row">
+                    <CustomFormField
+                        fieldType={FormFieldType.INPUT}
+                        control={form.control}
+                        name="emergencyContactName"
+                        label="Emergency Contact Name"
+                        placeholder="Guardian's name"
+                    />
+
+                    <CustomFormField
+                        fieldType={FormFieldType.PHONE_INPUT}
+                        control={form.control}
+                        name="emergencyContactNumber"
+                        label="Emergency Contact Number"
+                        placeholder="Emergency contact number"
+                        iconSrc="/assets/icons/user.svg"
+                        iconAlt="user"
+                    />
+                </div>
+
+                <section className="space-y-6">
+                    <div className="mb-9 space-y-1">
+                        <h2 className="sub-header">Medical Information</h2>
+                    </div>
+                </section>
+
+
+                <div className="flex flex-col gap-6 xl:flex-row">
+                    
+                </div>
+                
+                <SubmitButton isLoading={isLoading}>
+                    Get Started
+                </SubmitButton>
+            </form>
+        </Form>
+    )
+}
+
+export default RegisterForm
